d97591dbdfb71d6069b96849a63ad816868fb8d6
RamSailopal <thelinuxguys@yahoo.com>
2022-06-16 15:23:31 +0100

    Added a page to view individual users

--- django-front/newproject/newproject/urls.py
+++ django-front/newproject/newproject/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('user/<str:id>', views.user, name='user'),
 ]
--- django-front/newproject/newproject/views.py
+++ django-front/newproject/newproject/views.py
@@ -4,3 +4,7 @@ import requests, json
 def home(request):
    r = requests.get('http://django-test:8000/test/users/?format=json')
    return render(request, 'home.html', {'users': r.json()})
+def user(request, id):
+   r = requests.get('http://django-test:8000/test/users/' + id  + '/?format=json')
+   return render(request, 'user.html', {'user': r.json()})
+
